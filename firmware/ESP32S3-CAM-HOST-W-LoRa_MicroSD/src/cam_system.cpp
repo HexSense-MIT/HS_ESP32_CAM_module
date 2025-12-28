7cb6cd0e0863477e559e2b1b73ba3fdab062db1e
fangzheng liu <bitliufangzheng@gmail.com>
2025-12-27 19:47:45 -0500

    mac: working on the lora_microsd firmware, comm through lora and store images in microsd.

---
+++ firmware/ESP32S3-CAM-HOST-W-LoRa_MicroSD/src/cam_system.cpp
@@ -0,0 +1,37 @@
+#include "cam_system.h"
+
+void SD_init(void) {
+  // Enable internal pull-ups on SPI pins
+  pinMode(SD_SCK, INPUT_PULLUP);
+  pinMode(SD_MISO, INPUT_PULLUP);
+  pinMode(SD_MOSI, INPUT_PULLUP);
+  pinMode(SD_CS, INPUT_PULLUP);
+
+  SPI.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+
+  if (!SD.begin(SD_CS)) {
+    while (1);
+  }
+}
+
+void system_init(void) {
+  Serial1.begin(250000, SERIAL_8N1, CAM_RX_PIN, CAM_TX_PIN);
+
+  while (!Serial1) { delay(10); }
+
+  delay(100); // Allow some time for the system to stabilize
+
+  SD_init();        // Initialize the SD card
+
+  camadapter_init(); // Initialize the camera adapter
+  turnoffallcams();  // Ensure all cameras are off at startup
+
+  delay(100); // Allow some time for the system to stabilize
+
+  lora_setup(); // Initialize LoRa communication
+
+  // flush Serial1 to clear any initial data
+  while (Serial1.available()) {
+    Serial1.read(); // Clear any initial data in the buffer
+  }
+}
